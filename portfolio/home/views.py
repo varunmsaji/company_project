7062381178f62470784ce7be9a443daf31b194b9
varunmsaji <varunmsaji01@gmail.com>
2024-04-03 20:17:29 +0530

    initial commit with base template render

---
+++ portfolio/home/views.py
@@ -0,0 +1,9 @@
+from django.shortcuts import render,HttpResponse
+
+# Create your views here.
+
+
+def home(request):
+
+    # return HttpResponse("this is home page")  
+    return render(request,'index.html')
